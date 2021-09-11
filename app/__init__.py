e43fae91ad023f4ba261574549998bc2a1a3bd4e
ThanhTrungK15 <19502701.iuh@gmail.com>
2021-09-11 08:51:02 +0700

    Test jwt is ok

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
-app.config['SECRET_KEY'] = 'thisisthebiggestprojectever'
+
+# Setup the Flask-JWT-Extended extension
+jwt = JWTManager(app)
+
+# Secret key for jwt, need to be kept confidential
+app.config['SECRET_KEY'] = 'b61bbf5093e943859dcc1bbd17f1d5f0'
+
+# Number of minutes for access token exp
+app.config['TIME_TO_DIE'] = 30
+
+# local postgresql config database
+username = 'postgres'
+password = 'trung@2001'
+db_name = 'backend_runner'
+host = 'localhost'
+port = '5432'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{username}:{password}@{host}:{port}/{db_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# sqlite config database
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
+
 
 db = SQLAlchemy(app)
 
--- app/models.py
+++ app/models.py
@@ -4,18 +4,11 @@ from app import db
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
     # đây là option [khác, k13, k14, k15, k16, k17]
     course = db.String(db.String(10))
     is_super = db.Column(db.Boolean, default=False)
-
-
-class Token(db.Model):
-    __tablename__ = 'token'
-    id = db.Column(db.Integer, primary_key=True)
-    auth_token = db.Column(db.String(500), nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class Score(db.Model):
